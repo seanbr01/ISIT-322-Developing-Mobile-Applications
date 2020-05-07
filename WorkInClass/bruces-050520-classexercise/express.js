7ce6842e54cc51df14c2101df5c37ca79e4af0c8
Sean Bruce <seanbruce@hotmail.com>
2020-05-07 16:40:32 -0700

    in class work

--- WorkInClass/bruces-050520-classexercise/express.js
+++ WorkInClass/bruces-050520-classexercise/express.js
@@ -2,24 +2,12 @@ const http = require('http');
 const express = require('express');
 const app = express();
 
-/*
-const server = http.createServer((rep, res) => {
-    res.statusCode = 200;
-    res.setHeader( 'Content-Type', 'text/plain' );
-    res.end('This is the Entery Point for this app using Express.')
-});
-*/
+const publicRouter = require('./routes/public');
+const privateRouter = require('./routes/private');
 
-let indexPageHandler = (req, res) => {
-    res.end('First Response!');
-}
-
-app.get('/', indexPageHandler);
+app.use('/', publicRouter);
+app.use('/p', privateRouter);
 
 app.listen(3000, '127.0.0.1', () => {
     console.log('Server is running on socker 127.0.0.1:3000');
 });
-
-app.METHOD('url', () => {
-
-})
